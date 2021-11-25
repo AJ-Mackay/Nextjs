480307e78c70af7c4891cd38d3716a5e0511469a
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-25 11:53:01 +0000

    Adding Styles

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
 import Link from 'next/link';
 
+import styles from '../styles/Home.module.css';
+
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Stretch out on bed lick plastic bags but side-eyes your *jerk* other
         hand while being petted purr like an angel yet to pet a cat, rub its
-        belly, endure blood and agony, quietly weep, keep rubbing belly. Flex
-        claws on the human`s belly and purr like a lawnmower. Pooping rainbow
-        while flying in a toasted bread costume in space. Mew mew.
+        belly, endure blood and agony, quietly weep, keep rubbing belly.
+      </p>
+      <p className={styles.text}>
+        Flex claws on the human`s belly and purr like a lawnmower. Pooping
+        rainbow while flying in a toasted bread costume in space. Mew mew.
       </p>
       <Link href="/ninjas">
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
   );
