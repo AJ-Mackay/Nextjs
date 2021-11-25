4b87666df8e34f09cdfceec80fc08664d04a5e63
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-25 11:01:54 +0000

    Pages & Routes

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Kick up litter spill litter box, scratch at owner, destroy all
+        furniture, especially couch but stand with legs in litter box, but poop
+        outside chill on the couch table for cat slap dog in face pretend not to
+        be evil.
+      </p>
+      <p>
+        Let me in let me out let me in let me out let me in let me out who broke
+        this door anyway playing with balls of wool.
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,13 @@ export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+      <p>
+        Stretch out on bed lick plastic bags but side-eyes your *jerk* other
+        hand while being petted purr like an angel yet to pet a cat, rub its
+        belly, endure blood and agony, quietly weep, keep rubbing belly. Flex
+        claws on the human`s belly and purr like a lawnmower. Pooping rainbow
+        while flying in a toasted bread costume in space. Mew mew.
+      </p>
     </div>
   );
 }
