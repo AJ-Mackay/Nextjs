79b805c5e99bc1cc99844c3e558a3f399293bac7
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-25 11:12:55 +0000

    Adding Other Components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Stretch out on bed lick plastic bags but side-eyes your *jerk* other
@@ -9,6 +13,7 @@ export default function Home() {
         claws on the human`s belly and purr like a lawnmower. Pooping rainbow
         while flying in a toasted bread costume in space. Mew mew.
       </p>
+      <Footer />
     </div>
   );
 }
